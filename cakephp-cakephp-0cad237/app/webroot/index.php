549b5857cee516957510a6f964c9350ee8e83f0d
Daniel <d.hoarau@rt-iut.re>
2013-07-31 15:40:12 +0400

    ajout de la page d'accueil du site (fini 90 % manque logo et redirection vers page principale

--- cakephp-cakephp-0cad237/app/webroot/index.php
+++ cakephp-cakephp-0cad237/app/webroot/index.php
@@ -3,38 +3,42 @@ echo '
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-';
-?>
-<?php
-echo	$this->Html->css("bootstrap");
-?>
-<?php
-echo '
+	<link rel="stylesheet" type="text/css" href="css/accueil.css">
 	</head>
 	
 <body>
+<div class="global">
 <!-- ***************HEADER*************** -->
-
 	<div class="header">
-		<center> PROJET : SEABIRD </center> 
+		<h1 class="tete"><center>PROJET : SEABIRD</center></h1>  
 	</div>
 	
 <!-- ***************BODY*************** -->
 	
-	<div class="body">
-	 <div>
-	  <input type ="submit" value="entrer">
-	 <div>	
+	<div class="container">
+	
+	
+	
+	 <div class="logo">
+	  <img src="img/logo.png" >
+	 </div>
+	 <p class="intro">bienvenue sur Seabird!
+	accédez au site 
+	</p>
+	 <div class="enter">
+	  <input type="image" src="img/boutonEntrer.png" height="200px">
+	 </div>
+	
 	</div>
-
+	
 <!-- ***************FOOTER*************** -->
 	
-	<div class="footter">
-		<img> la mer </img>
+	<div class="footer">
+	<img src="img/UR.png" alt="univ" class="logU" width="100px" height="100px">
+	<img src="logo ecomar" alt="ecomar" class="logE">
 	</div>
-
+</div>
 </body>
 </html>
-
 ';
 ?>
